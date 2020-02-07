70b5152eb6d84edb6f4a8927d796b574af71fadc
jichenguang <45462001@qq.com>
2020-02-08 03:03:36 +0800

    1、修改settings的JAVA_COMPILE 2、调整对应maven的settings文件和依赖库文件

--- src/main/java/com/test/basic/practice/multiStatus/bone.java
+++ src/main/java/com/test/basic/practice/multiStatus/bone.java
@@ -3,7 +3,7 @@ package com.test.basic.practice.multiStatus;
 /**
  * Created by beigui on 2016/4/9.
  */
-public class Bone extends Food {
+class Bone extends Food {
 
     public void showname(){
         System.out.println("Bone");
